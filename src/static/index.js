8d597250e1575c71936567811f75a0d9bcf2df7a
Peter Damoc <pdamoc@gmail.com>
2017-05-05 13:33:20 +0300

    fixed errors parsing stored user

--- src/static/index.js
+++ src/static/index.js
@@ -3,29 +3,6 @@ require( './styles/main.scss' );
 // var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if jQuery not needed
 // require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed 
 
-var addEvent = (function () {
-  if (document.addEventListener) {
-    return function (el, type, fn) {
-      if (el && el.nodeName || el === window) {
-        el.addEventListener(type, fn, false);
-      } else if (el && el.length) {
-        for (var i = 0; i < el.length; i++) {
-          addEvent(el[i], type, fn);
-        }
-      }
-    };
-  } else {
-    return function (el, type, fn) {
-      if (el && el.nodeName || el === window) {
-        el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
-      } else if (el && el.length) {
-        for (var i = 0; i < el.length; i++) {
-          addEvent(el[i], type, fn);
-        }
-      }
-    };
-  }
-})();
 
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/Main' );
@@ -38,8 +15,9 @@ app.ports.saveUser.subscribe(function(user) {
     localStorage.setItem('elm-conduit-user', JSON.stringify(user));
 });
 
-addEvent(window, 'storage', function (event) {
-  if (event.key == 'elm-conduit-user') {
-    app.ports.userUpdates.send(event.newValue); 
+window.addEventListener('storage', function (event) {
+ console.log(event)
+ if (event.key == 'elm-conduit-user') {
+    app.ports.userUpdates.send(JSON.parse(event.newValue)); 
   }
 });
